e650bc1ff7ad259e2c5df9894aa0248b1160b587
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2019-03-22 07:49:21 +0200

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,5 @@
+/*const containerEl = document.querySelector('.gallery__images');
+const mixer = mixitup(containerEl);*/
+
+var containerEl = document.querySelector('.gallery__images');
+var mixer = mixitup(containerEl);
